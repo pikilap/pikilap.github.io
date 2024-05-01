e93a38702839aac89a195f9197becf38dc7ff8ba
snxdm <snxdma@outlook.com>
2024-05-01 14:12:56 +0800

    Site updated: 2024-05-01 14:12:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/clvn7utnj00007js6ht9gcw3f"];function toRandomPost(){
+var posts=["posts/2024/clvn7utnj00007js6ht9gcw3f"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
