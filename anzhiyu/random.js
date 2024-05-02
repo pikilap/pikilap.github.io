f6443eccad187125e8ab1231fbacedb2bf98d6aa
snxdm <snxdma@outlook.com>
2024-05-02 13:28:03 +0800

    Site updated: 2024-05-02 13:28:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/2024/clvn7utnj00007js6ht9gcw3f","posts/2024/clvos66nz0001its6cpvo72kk"];function toRandomPost(){
+var posts=["posts/2024/clvn7utnj00007js6ht9gcw3f"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
