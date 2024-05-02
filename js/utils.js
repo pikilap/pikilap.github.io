21d612f55e2ce65717876782f147bd1720a132c1
snxdm <snxdma@outlook.com>
2024-05-02 21:19:37 +0800

    Site updated: 2024-05-02 21:19:36

--- js/utils.js
+++ js/utils.js
@@ -845,8 +845,8 @@ const anzhiyu = {
       return;
     }
     const urlParams = new URLSearchParams(window.location.search);
-    const userId = "9216803294";
-    const userServer = "tencent";
+    const userId = "8152976493";
+    const userServer = "netease";
     const anMusicPageMeting = document.getElementById("anMusic-page-meting");
     if (urlParams.get("id") && urlParams.get("server")) {
       const id = urlParams.get("id");
